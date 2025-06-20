4c8b50158547960a4b19bde13eab27a11e8586b1
gouravindoriya <gouravindoriya00@gmail.com>
2025-06-20 17:30:29 +0530

    first time explore laravel

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>laravel day one </title>
+    <!-- <link rel="stylesheet" href="../css/app.css"> -->
+     @vite(['resources/css/app.css'])
+</head>
+<body class="h-full w-full bg-yellow-500 flex items-center justify-center flex-col">
+   
+    <h1> hello world!</h1>
+    <p>welcome to laravel</p>
+</body>
+</html>
